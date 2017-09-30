c0d503153d475dc3de2f169978f28d16eb363ebe
Narek Tutikyan <alnarek@gmail.com>
2017-09-30 19:47:24 +0400

    Socket implemented

--- doctor-calendar/src/app/notification/notification.component.ts
+++ doctor-calendar/src/app/notification/notification.component.ts
@@ -1,25 +1,43 @@
-import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild, OnDestroy} from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {ChatService} from "./notification.service";
 
 @Component({
+  moduleId: module.id,
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.css']
+  styleUrls: ['./notification.component.css'],
+  providers: [ChatService]
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent implements OnInit, OnDestroy {
 
-  constructor(private modal: NgbModal) {}
+  constructor(private modal: NgbModal, private chatService: ChatService) {}
   count: number = null;
+
+    messages = [];
+    connection;
+    message;
+
     @ViewChild('modalContent') modalContent: TemplateRef<any>;
 
   ngOnInit() {
-      setTimeout(()=> this.count++, 5000);
+      this.connection = this.chatService.getMessages().subscribe(message => {
+          this.messages.push(message);
+          console.log(message);
+          this.count++;
+      })
+      // setTimeout(()=> this.count++, 4000);
   }
 
   onNotificationClick (): void {
       if (this.count){
         this.modal.open(this.modalContent, { size: 'lg' });
       }
+      this.count=null;
 }
+
+    ngOnDestroy(): void {
+        this.connection.unsubscribe();
+    }
 
 }
